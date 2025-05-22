0272e2c3d8b0d088f9e2d679e079deb9f3912fe5
beidemariam22 <94792047+beidemariam22@users.noreply.github.com>
2025-05-22 12:30:00 +0200

    Update COVID Portfolio Project - Data Exploration.sql

--- SQL_Projects/COVID Portfolio Project - Data Exploration.sql
+++ SQL_Projects/COVID Portfolio Project - Data Exploration.sql
@@ -3,7 +3,7 @@ Covid 19 Data Exploration
 
 Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
 
-*/
+*/ 
 
 Select *
 From PortfolioProject..CovidDeaths
